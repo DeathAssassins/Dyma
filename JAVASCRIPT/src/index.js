b5652e5ea4ad9a6beabb123fd90daa8a23b19267
ainzer <tony.spicher@hotmail.com>
2024-12-21 23:31:17 +0100

    Dyma

---
+++ JAVASCRIPT/src/index.js
@@ -0,0 +1,9 @@
+export let a = 42;
+export let b = 22;
+export let result = a - b;
+
+export let c = "Essaye de ";
+export let d = "concaténer ";
+export let e = "plusieurs chaînes !";
+
+export let concatenation = c + d + e;
